031416a90520c5bd7ada11e1c4520b1978fce6fd
Yagiz Ulasoglu <yulasoglu@gmail.com>
2024-03-26 10:19:12 -0700

    Working on tests2

--- routes/users.test.js
+++ routes/users.test.js
@@ -29,23 +29,55 @@ describe("Users Routes Test", function () {
 
   /**GET / returns a list of all users */
 
-describe("GET /users", function () {
-  test("can see a list", async function () {
-    const response = await request(app)
-      .get(`/users`)
-      .query({ _token: testUserToken });
+  describe("GET /users", function () {
+    test("can see a list", async function () {
+      const response = await request(app)
+        .get(`/users`)
+        .query({ _token: testUserToken });
 
-    expect(response.statusCode).toEqual(200);
-    expect(response.body).toEqual({
-      users: [{
-        user: {
-          username: "test1",
-          password: "password",
-          first_name: "Test1",
-          last_name: "Testy1",
-          phone: "+14155550000",
-        }
-      }]
+      expect(response.statusCode).toEqual(200);
+      expect(response.body).toEqual({
+        users: [{
+            username: "test1",
+            first_name: "Test1",
+            last_name: "Testy1",
+        }]
+      });
     });
   });
+
+  /**Get / returns info about an user */
+
+  describe("GET /users/username", function () {
+    test("can see user info", async function() {
+      const response = await request(app)
+        .get(`/users/test1`)
+        .query({ _token: testUserToken });
+
+        expect(response.statusCode).toEqual(200);
+        expect(response.body).toEqual({
+          user: {
+            username: "test1",
+            first_name: "Test1",
+            last_name: "Testy1",
+            phone: "+14155550000",
+            join_at: expect.any(String),
+            last_login_at: null
+          }
+        })
+    })
+  })
+
+
+
+
+
+
+
+
+
+});
+
+afterAll(async function () {
+  await db.end();
 });
